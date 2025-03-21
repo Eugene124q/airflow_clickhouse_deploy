b80cd8396f67cd13da1bafb33f2393c6203ea92b
Eugene124q <applefaaan@gmail.com>
2025-03-21 10:08:09 +0000

    readme.md

--- dags/stocks.py
+++ dags/stocks.py
@@ -12,7 +12,7 @@ with DAG(
         #'retries': 1,
         'retry_delay': timedelta(minutes=1),
     },
-    schedule_interval='0 20 * * *',  # Каждый день в 23:00 МСК
+    schedule_interval='0 20 * * *',  # Каждый день в 23:00 МСК (от UTC)
     start_date=days_ago(1),  # DAG запускается с даты вчерашнего дня
     catchup=False,  # Не нагоняет пропущенные выполнения
     tags=['wildberries', 'stocks'],
@@ -21,8 +21,8 @@ with DAG(
     # Операция: запуск shell-скрипта
     run_script = BashOperator(
         task_id="run_wildberries_stocks",
-        bash_command="/opt/airflow/projects/wildberries/wildberries_stock.py",  # Укажите полный путь к скрипту
+        bash_command="/opt/airflow/projects/wildberries/wildberries_stock.py",
         do_xcom_push=False,
     )
 
-    run_script  # Запуск задачи
+    run_script  
